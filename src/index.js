69f37a949235be3de0daff9adf13553caf3c7fad
Gabriel Maine <gabrielmaine14@gmail.com>
2024-03-15 15:15:27 -0300

    feat: remove socket.io for vercel compatibility

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import compression from 'express-compression'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
 import handlebars from 'express-handlebars'
-import { Server } from 'socket.io'
 import { router as productRouter } from './routes/Product.routes.js'
 import { router as cartRouter } from './routes/Cart.routes.js'
 import { router as viewsRouter } from './routes/Views.routes.js'
 import { router as sessionRouter } from './routes/Session.routes.js'
 import { router as userRouter } from './routes/Users.routes.js'
 import { connectDB } from './config/dbConnection.js'
-import { messageModel } from './dao/mongo/models/Messages.model.js'
-import productService from './services/Products.service.js'
 import passport from 'passport'
 import initializedPassport from './config/passport.config.js'
 import errorHandler from './services/errors/index.js'
@@ -27,7 +24,6 @@ import cookieParser from 'cookie-parser'
 const app = express()
 const port = process.env.PORT || 8080
 const httpServer = app.listen(port, () => console.log(`Servidor en el puerto ${port}`))
-const socketServer = new Server(httpServer)
 
 const swaggerOptions = {
     definition: {
@@ -84,62 +80,3 @@ app.use('/api/carts', cartRouter)
 app.use('/', viewsRouter)
 app.use('/api/sessions', sessionRouter)
 app.use('/api/users', userRouter)
-
-socketServer.on('connection', socket => {
-    console.log('Nuevo cliente conectado')
-    socket.on('message', data => {
-        console.log(data)
-    })
-    socket.on('new-product', async data => {
-        let products = await productService.getAllProducts()
-        try {
-            await productService.createProduct(data)
-            console.log('Nuevo producto')
-            products = await productService.getAllProducts()
-            console.log(products)
-            socket.emit('reRender-products', products)
-        } catch (error) {
-            console.log(error.message)
-            console.log('No se pudo cargar el producto')
-            socket.emit('reRender-products', products)
-        }
-    })
-    socket.on('delete-product', async data => {
-        let products = await productService.getAllProducts()
-        try {
-            await productService.deleteProduct(data)
-            console.log('Producto borrado')
-            products = await productService.getAllProducts()
-            socket.emit('reRender-products', products)
-        } catch (error) {
-            console.log('No se pudo borrar el producto')
-            socket.emit('reRender-products', products)
-        }
-    })
-    socket.on('new-message', async data => {
-        const addMessage = await messageModel.create(data)
-        socket.emit('reRender-chat', addMessage)
-    })
-    socket.on('change-page', async data => {
-        let products = await productService.paginateProducts({}, { limit: 5, page: data, lean: true })
-
-        const results = {
-            status: products.docs.length > 0 ? 'success' : 'error',
-            payload: products.docs,
-            totalPages: products.totalPages,
-            prevPage: products.prevPage,
-            nextPage: products.nextPage,
-            page: products.page,
-            hasPrevPage: products.hasPrevPage,
-            hasNextPage: products.hasNextPage,
-            prevLink: products.hasPrevPage
-                ? `http://localhost:8080/api/products?limit=${products.limit}&page=${products.prevPage}`
-                : null,
-            nextLink: products.hasNextPage
-                ? `http://localhost:8080/api/products?limit=${products.limit}&page=${products.nextPage}`
-                : null,
-        }
-
-        socket.emit('reRender-page', results)
-    })
-})
